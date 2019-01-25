0cb021b027667e33caa9e64c089df63b9a2b0d45
somayeh khavari <46685906+somayehkhavari@users.noreply.github.com>
2019-01-25 15:32:31 +0330

    Add files via upload

---
+++ Increment.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<conio.h> 
+using namespace std;
+	int increment(int);
+		int x=3;
+			int main(){
+				x=increment(x);
+					cout<<x;
+				x=increment(x);
+					cout<<x;
+		}	
+				int increment(int a){
+				return a;
+			}
